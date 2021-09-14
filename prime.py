f008f88fed1f89435d701517fbb6545ef61ba177
ROHAN <75291986+rohan37-pro@users.noreply.github.com>
2021-09-14 02:27:35 -0400

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,22 @@
+class prime:
+
+	def check_prime(self,num):
+		for i in range(2,num):
+			if num%i == 0:
+				print(f"sorry broo!!!! \nthe number {num} is not a prime number")
+				break
+		else:
+			print(f"number {num} is prime ###~~~")
+
+
+
+
+while True:
+	mode = input("what do you want check prime(p)/quit(q) : ")
+	if mode == 'p':
+		num = int(input("enter a number : "))
+		prime123 = prime()
+		prime123.check_prime(num)
+
+	if mode == "q":
+		break
