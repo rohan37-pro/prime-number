e00a51d3cb706e320dcff47a9c9d5cf5effea0c9
ROHAN <75291986+rohan37-pro@users.noreply.github.com>
2021-10-03 03:50:23 -0400

    Update largest-prime.py

--- largest-prime.py
+++ largest-prime.py
@@ -1,4 +1,5 @@
 
+
 class prime:
 
 	def __init__(self,number):
@@ -7,14 +8,13 @@ class prime:
 			self.not_prime = self.check(number)
 			if self.not_prime == True:
 				number -= 1
-
 		self.large_prime = number
 		
 
 	def check(self,number):
 		if number%2==0:
 			return True
-		for i in range(3,int(number**(1/2))+1,2)	:
+		for i in range(3,int(number**(1/2))+1,2):
 			if number%i==0:
 				return True
 		else:
@@ -26,3 +26,4 @@ if __name__ == "__main__":
 	number_ = int(input("enter a number : "))
 	prime_ = prime(number_)
 	print(f"largest prime is {prime_.large_prime}")
+	
